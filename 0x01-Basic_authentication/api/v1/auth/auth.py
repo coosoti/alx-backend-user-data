6983be951dad6638c0341aae42f1106f096d2b62
coosoti <charlesosoti@gmail.com>
2022-01-11 19:34:12 +0300

    refactor - added docs

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,12 +9,15 @@ from typing import List, TypeVar
 class Auth:
     """Auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """this function ensures authentication"""
         return False
 
 
     def authorization_header(self, request=None) -> str:
+        """this function add authorization header"""
         return None
 
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """this method gets the current user"""
         None
