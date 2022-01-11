1f070c2934f222d509e18d765716a5e6615109ad
coosoti <charlesosoti@gmail.com>
2022-01-11 19:38:07 +0300

    refactor - added docs

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,11 +12,9 @@ class Auth:
         """this function ensures authentication"""
         return False
 
-
     def authorization_header(self, request=None) -> str:
         """this function add authorization header"""
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """this method gets the current user"""
