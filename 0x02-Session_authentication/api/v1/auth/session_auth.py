a0ab85bdf2c93c4fe498453b9a24700072dc6567
coosoti <charlesosoti@gmail.com>
2022-01-13 12:50:39 +0300

    [Create a session] added an instance method def create_session(self, user_id: str = None) -> str: that creates a Session ID for a user_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,8 +5,16 @@ Session Authentication module
 
 from api.v1.auth.auth import Auth
 from models.user import User
-
+from uuid import uuid4
 
 class SessionAuth(Auth):
     """SessionAuth class"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """this method creates a session ID for a user"""
+        if user_id is None or type(user_id) != str:
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
