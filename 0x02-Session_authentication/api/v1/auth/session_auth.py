d8a7c807c7c6dc32dc1cc197bbb1531c87b8e34f
coosoti <charlesosoti@gmail.com>
2022-01-13 12:36:44 +0300

    [Empty Session] added empty class SessionAuth that inherits from Auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Session Authentication module
+"""
+
+from api.v1.auth.auth import Auth
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """SessionAuth class"""
+    pass
