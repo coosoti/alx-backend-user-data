fd1841d90f3479c9dfc3fc7e065f218087567b65
coosoti <charlesosoti@gmail.com>
2022-01-13 12:57:43 +0300

    [User ID for Session ID] added an instance method def user_id_for_session_id(self, session_id: str = None) -> str: that returns a User ID based on a Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,6 +7,7 @@ from api.v1.auth.auth import Auth
 from models.user import User
 from uuid import uuid4
 
+
 class SessionAuth(Auth):
     """SessionAuth class"""
     user_id_by_session_id = {}
@@ -18,3 +19,9 @@ class SessionAuth(Auth):
         session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """this method returns a User ID based on a Session ID"""
+        if session_id is None or type(session_id) != str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
