5eab14742aeb45a39347c0174fa920c433eb7f3c
coosoti <charlesosoti@gmail.com>
2022-01-19 14:53:37 +0300

    [Register User] added a method that saves the user to the database using self._db and return the User object.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,8 +4,31 @@ User Authentication module
 """
 
 from bcrypt import hashpw, gensalt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
     """this method takes password and return salted hash of the password"""
     return hashpw(password.encode('utf-8'), gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """this method create new user and save to the db and return
+        User object"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
