825750b88d9c20a718865010412381cbb7603d2a
coosoti <charlesosoti@gmail.com>
2022-01-20 12:55:13 +0300

    [Generate reset password token] added a method Auth.get_reset_password_token method. It take an email string argument and returns a string - Find the user corresponding to the email. If the user does not exist, raise a ValueError exception. If it exists, generate a UUID and update the user’s reset_token database field. Return the token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -80,3 +80,14 @@ class Auth:
             self._db.update_user(user.id, session_id=None)
         except NoResultFound:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """this method locate user by email. if user exists, generates a
+        UUID and update the user's reset token db field. then return token"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
