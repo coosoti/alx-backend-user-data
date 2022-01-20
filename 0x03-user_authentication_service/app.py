fd3b11d42aa1534c5ebea97fa757c6bd8aea6a63
coosoti <charlesosoti@gmail.com>
2022-01-20 11:35:41 +0300

    [User Log In] added a function that creates a new session for the user, store it the session ID as a cookie with key session_id on the response and return a JSON payload

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 API Route Module
 """
 
-from flask import Flask, jsonify, request
+from flask import Flask, abort, jsonify, request
 from auth import Auth
 
 AUTH = Auth()
@@ -33,6 +33,22 @@ def users() -> str:
         return jsonify({
             "message": "email already registered"
         }), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """this function creates a new session for the user, store it in the
+    session ID as a cookie with key "session_id" and returns JSON payload"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    valid_login = AUTH.valid_login(email, password)
+
+    if not valid_login:
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": f"{email}", "message": "logged in"})
+    response.set_cookie('session_id', session_id)
+    return response
 
 
 if __name__ == "__main__":
